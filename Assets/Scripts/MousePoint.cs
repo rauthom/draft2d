0fd35f50668e569cdd50f29bdd1f5fa54877296a
rauthom <robin.authom@student.uclouvain.be>
2015-01-02 16:52:22 +0100

    Test 2

---
+++ Assets/Scripts/MousePoint.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MousePoint : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 nextPos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		nextPos.z = 0.0f;
+		transform.position = nextPos;
+	}
+}
