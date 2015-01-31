9b128d7ed4a1bc3d9cd311bd074f0fa66c7807d0
rauthom <robin.authom@student.uclouvain.be>
2015-01-31 13:22:52 +0100

    Communication Client => Unity

--- Assets/Scripts/BlueColor.cs
+++ Assets/Scripts/BlueColor.cs
@@ -9,7 +9,7 @@ public class BlueColor : MonoBehaviour
 		// Use this for initialization
 		void Start ()
 		{
-				renderer.material.color = new Color (0.20f, 0.50f, 0.80f);
+				renderer.material.color = new Color (0.50f, 0.50f, 0.80f);
 		}
 	
 		// Update is called once per frame
---
+++ Assets/Scripts/InputCubeContact.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class InputCubeContact : MonoBehaviour
+{
+		public static bool contact = false;
+		public Text impactTxt;
+	
+		// Use this for initialization
+		void Start ()
+		{
+		
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				rigidbody.inertiaTensor = Vector3.zero;
+				rigidbody.angularVelocity = Vector3.zero;
+				rigidbody.velocity = Vector3.zero;
+		}
+	
+		void OnCollisionEnter (Collision collision)
+		{
+				impactTxt.text = "Okay";
+				if (collision != null && collision.contacts != null && collision.contacts.Length > 0) {
+						Vector3 contactPoint = collision.contacts [0].point;
+						impactTxt.text = "Last impact at: (" + contactPoint.x.ToString () + "," + contactPoint.z.ToString () + ")";
+						collision.collider.gameObject.renderer.material.color = Color.yellow;
+						contact = true;
+				}
+				
+		}
+	
+		void OnCollisionExit (Collision collision)
+		{
+				collision.collider.gameObject.renderer.material.color = Color.red;
+				contact = false;
+		}
+}
